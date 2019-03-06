ff6df1a9d42fa02b940b6813db4a229406d85404
Orvin Savero <orvinsavero@yahoo.com>
2019-03-06 16:34:24 +0700

    javascript3

---
+++ Javascript3.js
@@ -0,0 +1,44 @@
+var day = 1; 
+var month = 2; 
+var year = 2019;
+
+switch (month) {
+  case 1:
+  month = "January";
+    break;
+  case 2:
+  month = "February";
+    break;
+  case 3:
+  month = "March";
+    break;
+  case 4:
+  month = "April";
+    break;
+  case 5:
+  month = "May";
+    break;
+  case  6:
+  month = "June";
+    break;
+  case  7:
+  month = "July";
+    break;
+  case  8:
+  month = "August";
+    break;
+  case  9:
+  month = "September";
+    break;
+  case  10:
+  month = "October";
+    break;
+  case  11:
+  month = "November";
+    break;
+  case  12:
+  month = "December";
+}
+
+console.log(day + " " + month + " " + year)
+
